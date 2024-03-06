cd49f5e67511bc95355f52fa30ec78f2cf7ef36e
nchan315 <nchan315@student.ubc.ca>
2024-03-05 23:40:17 -0800

    added chars up to Rigor

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const gameData = {
+    "Chrome: Arclight": "audio/Arclight BA.mp3",
+    "Watanabe: Astral": "audio/Astral BA.mp3",
     "Kamui: Bastion": "audio/Bastion BA.mp3",
+    "Karenina: Blast": "audio/Blast BA.mp3",
     "Ayla: Brilliance": "audio/Brilliance BA.mp3",
+    "Alpha: Crimson Abyss": "audio/Crimson Abyss BA.mp3",
+    "Camu: Crocotta": "audio/Crocotta BA.mp3",
+    "Lucia: Dawn": "audio/Dawn BA.mp3",
     "Liv: Eclipse": "audio/Eclipse BA.mp3",
+    "Karenina: Ember": "audio/Ember BA.mp3",
+    "Lee: Entropy": "audio/Entropy BA.mp3",
     "Lucia: Lotus": "audio/Lotus BA.mp3",
+    "Liv: Luminance": "audio/Luminance BA.mp3",
+    "Liv: Lux": "audio/Lux BA.mp3",
     "Watanabe: Nightblade": "audio/Nightblade BA.mp3",
+    "Lee: Palefire": "audio/Palefire BA.mp3",
+    "Lucia: Plume": "audio/Plume BA.mp3",
+    "Nanami: Pulse": "audio/Pulse BA.mp3",
+    "Rosetta: Rigor": "audio/Rigor BA.mp3",
+    "Vera: Rozen": "audio/Rozen BA.mp3",
+    "Sophia: Silverfang": "audio/Silverfang BA.mp3",
     "Nanami: Storm": "audio/Storm BA.mp3",
+    "Kamui: Tenebrion": "audio/Tenebrion BA.mp3",
+    "Bianca: Veritas": "audio/Veritas BA.mp3",
     "Bianca: Zero": "audio/Zero BA.mp3"
 }
 const charKeys = Object.keys(gameData);
